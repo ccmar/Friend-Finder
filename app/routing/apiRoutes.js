bbc68fe08220835e34d581d333575d757a50ffe7
Colton Cmar <colton@David-Silbermanns-MacBook-Pro.local>
2017-10-21 19:40:49 -0400

    updates to Friend Finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,55 +1,41 @@
-// var friends = require("../data/friends");
-// module.exports = function(app)  {
-//   app.get("/api/friends", function(req, res)  {
-//     res.json(friends);
-//     });
-//
-//   app.post("/api/friends", function(req, res)  {
-//     var bestMatch ={
-//       name: "",
-//       photo: "",
-//       friendDifference: Infinity
-//       };
-//   var userData = req.body;
-//   var userScores = userData.scores;
-//   var totalDifference;
-//
-//   for (var i = 0; i < friends.length; i++)  {
-//     var currentFriend = friends[i];
-//     totalDifference = 0;
-//
-//     console.log(currentFriend.name);
-//
-//   for (var j = 0; j < currentFriend.scores.length; j++)  {
-//     var currentFriendScore = currentFriend.scores[j];
-//     var currentUserScore = userScores[j];
-//
-//     totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
-//   }
-//
-//   if (totalDifference <= bestMatch.friendDifference)  {
-//     bestMatch.name = currentFriend.name;
-//     bestMatch.photo = currentFriend.photo;
-//     bestMatch.friendDifference = totalDifference;
-//     }
-//   }
-//
-//   friends.push(userData);
-//
-//   res.json(bestMatch);
-//   });
-// };
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
+module.exports = function(app)  {
+  app.get("/api/friends", function(req, res)  {
+    res.json(friends);
+    });
 
-module.exports = function(app) {
+  app.post("/api/friends", function(req, res)  {
+    var bestMatch ={
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+      };
+  var userData = req.body;
+  var userScores = userData.scores;
+  var totalDifference;
 
-  app.get("api/friends", function(req, res) {
-    res.json(friendData);
-    res.send(friendData);
-  });
+  for (var i = 0; i < friends.length; i++)  {
+    var currentFriend = friends[i];
+    totalDifference = 0;
 
-  app.post("api/friends", function(req, res) {
-    friendData.push(req.body);
-    res.json(true);
+    console.log(currentFriend.name);
+
+  for (var j = 0; j < currentFriend.scores.length; j++)  {
+    var currentFriendScore = currentFriend.scores[j];
+    var currentUserScore = userScores[j];
+
+    totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+  }
+
+  if (totalDifference <= bestMatch.friendDifference)  {
+    bestMatch.name = currentFriend.name;
+    bestMatch.photo = currentFriend.photo;
+    bestMatch.friendDifference = totalDifference;
+    }
+  }
+
+  friends.push(userData);
+
+  res.json(bestMatch);
   });
 };
